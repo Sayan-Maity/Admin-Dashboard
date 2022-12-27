a2701e63ad0115c7395bbe2eb17f10029b14760b
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-27 17:08:33 +0530

    different components are modified and added

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 All rights reserved | Made with by 🤍 by<a href="https://github.com/Sayan-Maity"> <p className='underline'>Sayan Maity</p></a>
+    </p>
+  </div>
+);
+
+export default Footer;
